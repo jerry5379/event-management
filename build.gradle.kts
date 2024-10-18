07d76138f815eaa27011a1401298f52eb0c68e11
jerry5379 <kadampritesh46@gmail.com>
2024-10-18 19:56:43 +0530

    updated the password feild

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,16 +30,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
-
 // https://mvnrepository.com/artifact/org.springdoc/springdoc-openapi-starter-webmvc-ui
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
-
-
-//	implementation("org.springframework.boot:spring-boot-starter-actuator")
-//	implementation("org.springframework.boot:spring-boot-starter-security")
-//	implementation("io.springfox:springfox-swagger-ui:3.0.0")
-//	implementation("io.springfox:springfox-swagger2:3.0.0")
-//	implementation("javax.servlet:javax.servlet-api:4.0.1")
 	implementation("io.jsonwebtoken:jjwt:0.9.1")
 	implementation("javax.xml.bind:jaxb-api:2.3.1")
 	implementation("com.sun.xml.bind:jaxb-core:2.3.0")
--- src/main/java/com/eventmanagement/authentication/models/UserRegistrationDTO.java
+++ src/main/java/com/eventmanagement/authentication/models/UserRegistrationDTO.java
@@ -3,6 +3,7 @@ package com.eventmanagement.authentication.models;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,10 @@ public class UserRegistrationDTO {
     private String email;
     @NotBlank
     @NotNull
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,20}$",
+            message = "Password must be 8-20 characters long, contain at least one digit, one uppercase letter, one lowercase letter, and one special character"
+    )
     private String password;
     @NotBlank
     private String confirmPassword;
